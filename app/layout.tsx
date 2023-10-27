e1d7a1de6d3a735a48c69cbd966057a5985e961f
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-10-28 01:41:18 +0400

    configured next-auth with github provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { NextAuthProvider } from "@/components/NextAuthProvider";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,11 +22,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased p-8",
           fontSans.variable
         )}
       >
-        {children}
+        <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import Header from "@/components/header/header";
 import { Button } from "@/components/ui/button";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <main className="p-8">
-      <Button>btn</Button>
-    </main>
+    <>
+      <Header />
+      <main>
+        <Button>btn</Button>
+      </main>
+
+      {session?.user ? (
+        <>
+          Signed in as {session.user?.name} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+    </>
   );
 }
