cb5a938b6c435242d5279e8e0ab27aee65452c97
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-10-28 01:48:18 +0400

    assigning user unique id when logged in, with session callback

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,24 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      try {
+        const userID = token.sub;
+
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            userID,
+          },
+        };
+      } catch (err) {
+        console.log(err);
+        return session;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
