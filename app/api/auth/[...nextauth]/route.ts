063321ec50c4fc0440c2bae3eab02c24445aa7bd
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-10-30 22:45:36 +0400

    added google auth provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { mongoDBService } from "@/utils/mongoDBService";
 
 const handler = NextAuth({
@@ -7,6 +8,10 @@ const handler = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
   callbacks: {
